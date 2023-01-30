a82d4d299bbf8a0486f3fefa5eee1596e6ee346b
mikun <mikun@gmail.com>
2023-01-29 23:36:54 -0800

    added more content

--- src/Comp/Home/Industries/industries.jsx
+++ src/Comp/Home/Industries/industries.jsx
@@ -56,56 +56,109 @@ const Industries = () => {
   return (
     <div>
       <Slide bg="#EDF0FF">
-        <H1 align="center" fs="48px" pad="28px 0">Industries We Innovate</H1>
+        <H1 align="center" fs="48px" pad="28px 0">
+          Industries We Innovate
+        </H1>
 
         <div>
-        <Slider {...settings}>
-          <Sector>
-            <div>
-              <Icon
-                src="./Images/Agriculture Icon.svg"
-                alt="Agriculture Icon"
-                width="100.16px"
-                height="100.19px"
-              />
-            </div>
-            <P fs="24px" pad="12px 0" style={{margin: "auto"}}>Agriculture</P>
+          <Slider {...settings}>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/Agriculture Icon.svg"
+                  alt="Agriculture Icon"
+                  width="100.16px"
+                  height="100.19px"
+                />
+              </div>
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Agriculture
+              </P>
 
-            <button>Learn more</button>
-          </Sector>
-          <Sector>
-            <div>
-              <Icon
-                src="./Images/radar.svg"
-                alt="radar"
-                width="120.66px"
-                height="110.43px"
-              />
-            </div>
+              <button>Learn more</button>
+            </Sector>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/radar.svg"
+                  alt="radar"
+                  width="120.66px"
+                  height="110.43px"
+                />
+              </div>
 
-            <P fs="24px" pad="12px 0" style={{margin: "auto"}}>Telecommunications</P>
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Telecommunications
+              </P>
 
-            <button>Learn more</button>
-          </Sector>
-          <Sector>
-            <div>
-              <Icon src="./Images/Vector.svg" alt="Heart Icon" width="120px" height="110.33px"/>
-            </div>
+              <button>Learn more</button>
+            </Sector>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/Vector.svg"
+                  alt="Heart Icon"
+                  width="120px"
+                  height="110.33px"
+                />
+              </div>
 
-            <P fs="24px" pad="12px 0" style={{margin: "auto"}}>Healthcare</P>
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Healthcare
+              </P>
 
-            <button>Learn more</button>
-          </Sector>
-          <Sector>
-            <div>
-              <Icon src="./Images/Energy.svg" alt="Energy" width="113.58" height="113.64px"/>
-            </div>
+              <button>Learn more</button>
+            </Sector>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/Energy.svg"
+                  alt="Energy"
+                  width="113.58"
+                  height="113.64px"
+                />
+              </div>
 
-            <P fs="24px" pad="12px 0" style={{margin: "auto"}}>Energy Productions</P>
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Energy Productions
+              </P>
 
-            <button>Learn more</button>
-          </Sector>
-        </Slider>
+              <button>Learn more</button>
+            </Sector>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/waste.png"
+                  alt="waste management"
+                  width="113.58"
+                  height="113.64px"
+                />
+              </div>
+
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Waste Management
+              </P>
+
+              <button>Learn more</button>
+            </Sector>
+            <Sector>
+              <div>
+                <Icon
+                  src="./Images/robot.png"
+                  alt="robotics"
+                  width="113.58"
+                  height="113.64px"
+                />
+              </div>
+
+              <P fs="24px" pad="12px 0" style={{ margin: "auto" }}>
+                Robotics
+              </P>
+
+              <button>Learn more</button>
+            </Sector>
+            
+          </Slider>
         </div>
       </Slide>
     </div>
